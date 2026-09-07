27c762243c388612cd980562ee9d4ff4d07c1739
baobao739 <akkosyn@gmail.com>
2026-09-06 19:08:13 -0500

    Fix access status response for access gate

--- netlify/functions/access-status.js
+++ netlify/functions/access-status.js
@@ -3,5 +3,5 @@ const { getCookie, verifyToken, json } = require("./_shared/auth");
 exports.handler = async (event) => {
   if (event.httpMethod !== "GET") return json(405, { error: "Method not allowed" });
   const valid = await verifyToken(getCookie(event, "akkoflac_access"), "access");
-  return json(200, { unlocked: valid }, { "Cache-Control": "no-store" });
+  return json(200, { valid, unlocked: valid }, { "Cache-Control": "no-store" });
 };
